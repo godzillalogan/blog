96409823f8522895e77b217596bef9122b3fdbbd
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2022-01-18 13:44:00 +0800

    feat:add warning_msg to signin page

--- config/passport.js
+++ config/passport.js
@@ -8,16 +8,21 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ 
+    usernameField: 'email', 
+    passwordField: 'password',
+    passReqToCallback: true, //當passReqToCallback設定為true時，request object將作為參數傳入的callback function，這表示當LocalStrategy呼叫 callback時，可以透過參數傳遞得到 req物件控制權。
+    session: false
+  }, (req,email, password, done) => { //加上req
     User.findOne({ email })
       .then(user => {
         if (!user) { //user不存在
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false,  req.flash("warning_msg", "This email is not registered! (✖╭╮✖)"))
         }
         //第一個參數是使用者的輸入值，而第二個參數是資料庫裡的雜湊值，bcrypt 會幫我們做比對，並回傳布林值，在文中我們用 isMatch 來代表。
         return bcrypt.compare(password,user.password).then(isMatch =>{
           if(!isMatch){
-            return done(null, false, {message:'Email or Password incorrect.'})
+            return done(null, false,  req.flash("warning_msg", "Email or Password incorrect. (✖﹏✖)"))
           }
           return done(null, user)
         })

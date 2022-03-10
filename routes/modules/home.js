bffce27ff3db46a0a5752b17aed7b5c6fc9f2c08
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2022-03-10 21:26:09 +0800

    feat: add footer in index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category');
 const Article = require('../../models/article')
 
 router.get('/', async (req,res)=>{
+  const categories = await Category.find().lean().sort({createdAt:'desc'})
   const articles = await Article.find().lean()
   .sort({createdAt:'desc'})
-  res.render('index',{ articles })
+  res.render('index',{ articles, categories })
 })
 
 
